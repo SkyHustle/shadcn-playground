93532c0df658891efd7fb568d6f45c65d22f08db
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-14 13:29:51 -0700

    Differentiating peers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,17 +42,42 @@ export default function Home() {
                 </p>
             </a>
 
-            <form>
-                <label className="block">
-                    <span className="block text-sm font-medium text-slate-700">
-                        Email
-                    </span>
-                    <input type="email" className="... peer" />
-                    <p className="invisible mt-2 text-sm text-pink-600 peer-invalid:visible">
-                        Please provide a valid email address.
-                    </p>
+            <fieldset className="mx-auto max-w-md bg-white p-8 text-sm shadow">
+                <div className="mb-6 border-b border-slate-200 pb-2 text-base font-semibold">
+                    Published status
+                </div>
+                <input
+                    id="draft"
+                    className="peer/draft form-radio mb-0.5 mr-2 border-slate-300 text-sky-400 focus:ring-sky-300"
+                    type="radio"
+                    name="status"
+                    defaultChecked
+                />
+                <label
+                    className="font-medium peer-checked/draft:text-sky-500"
+                    htmlFor="draft"
+                >
+                    Draft
+                </label>{" "}
+                <input
+                    id="published"
+                    className="peer/published form-radio mb-0.5 ml-4 mr-2 border-slate-300 text-sky-400 focus:ring-sky-300"
+                    type="radio"
+                    name="status"
+                />
+                <label
+                    className="font-medium peer-checked/published:text-sky-500"
+                    htmlFor="published"
+                >
+                    Published
                 </label>
-            </form>
+                <div className="mt-4 hidden text-gray-500 peer-checked/draft:block">
+                    Drafts are only visible to administrators.
+                </div>
+                <div className="mt-4 hidden text-gray-500 peer-checked/published:block">
+                    Your post will be publicly visible on your site.
+                </div>
+            </fieldset>
 
             <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
                 <a
