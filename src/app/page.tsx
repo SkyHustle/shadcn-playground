2052b470292e8bac4a0c26c368b10bf6320adefb
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-19 12:26:05 -0700

    Sticky navbar with light/dark mode toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import React from "react"
 import { ModeToggle } from "@/components/ui/mode-toggle"
+import { NavigationMenuDemo } from "@/components/NavigationMenuDemo"
 
 export default function CardsChat() {
     return (
-        <div className="flex h-screen items-center justify-center">
+        // create a sticky nav bar
+        <nav className="sticky top-0 z-50 flex items-center justify-between border-b border-border bg-background px-4 py-2">
+            <NavigationMenuDemo />
             <ModeToggle />
-        </div>
+        </nav>
     )
 }
